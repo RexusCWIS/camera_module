2038e871324e5afee9a64e72ecf9f43c6abb97d7
Olivier Desenfans <desenfans.olivier@gmail.com>
2013-10-28 11:50:44 +0200

    The camera initialisation now retrieves informations about the device.

--- include/camera.hpp
+++ include/camera.hpp
@@ -7,18 +7,32 @@
 #define DEF_CAMERA_HPP
 
 #include "types.hpp"
+#include <uEye.h>
+
+#define ERROR_CANT_RETRIEVE_INFO    (-1)
+#define ERROR_CANT_INIT_CAMERA      (-2)
 
 class Camera {
     
     public: 
         Camera(); 
         ~Camera();
-        
+ 
+        /**
+         * @brief Detects the number of uEye cameras connected to the system.
+         * @returns Number of connected cameras. Returns -1 if it was not possible to retrieve this information. 
+         */
+        static Int32_t getNumberOfCameras(void); 
+
         /**
          * @brief Initializes the camera. 
          * @details This function tries to initialize the camera. 
          */
         UInt32_t init(void); 
+
+    private: 
+        HIDS camID; 
+        UEYE_CAMERA_LIST *camInfo; 
 
 };
 
--- src/camera.cpp
+++ src/camera.cpp
@@ -5,17 +5,59 @@
 
 #include "camera.hpp"
 #include <uEye.h>
+#include <iostream>
+
+static inline void printCamInfo(UEYE_CAMERA_INFO *uci) {
+    
+    cout << "Camera ID:\t" << uci->dwCameraID << "\n" <<
+            "Device ID:\t" << uci->dwDeviceID << "\n\n" <<
+            "Camera model:\t" << uci->Model << "\n" << 
+            "Serial number:\t" << uci->SerNo << "\n";
+}
+
+Int32_t Camera::getNumberOfCameras(void) {
+
+    INT nbOfCams = 0;
+    INT status = is_GetNumberOfCameras(&nbOfCams);
+
+    if (status == IS_INVALID_PARAMETER) {
+        nbOfCams = -1; 
+    }
+
+    return (Int32_t) nbOfCams; 
+}
 
 UInt32_t Camera::init(void) {
 
-    HIDS camID = 0;
-    INT status = is_InitCamera(&camID, NULL);
+    INT status = 0;
+
+    /* Allocate memory for the camera info fields */
+    this->camInfo = (UEYE_CAMERA_LIST *) new UInt8_t[sizeof(ULONG) + sizeof(UEYE_CAMERA_INFO)];
+    /* We work with a single camera connected to the system */
+    this->camInfo->dwCount = 1;  
+
+    status = is_GetCameraList(this->camInfo);
+    if (status != IS_SUCCESS) {
+        return ERROR_CANT_RETRIEVE_INFO; 
+    }
+
+    printCamInfo(&(this->camInfo.uci));
+
+    this->camID = camInfo->uci[0].dwCameraID;
+    status = is_InitCamera(&this->camID, NULL);
 
     if (status != IS_SUCCESS) {
-
-        return -1;  
+        return ERROR_CANT_INIT_CAMERA;  
     }
 
     return 0; 
+}
+
+Camera::~Camera() {
+
+    INT status = is_ExitCamera(this->camID);
+    (void) status;
+
+    delete [] camInfo; 
 }
 
