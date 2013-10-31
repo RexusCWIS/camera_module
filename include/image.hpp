c63f0297461eb945151cb16d48db68981f649375
Olivier Desenfans <desenfans.olivier@gmail.com>
2013-10-31 12:18:49 +0100

    Implemented the Image class constructor and destructor.

--- include/image.hpp
+++ include/image.hpp
@@ -6,19 +6,24 @@
 #ifndef DEF_IMAGE_HPP
 #define DEF_IMAGE_HPP
 
+/** @brief Modular type for the pixel size. */
+typedef char pixel_t; 
+
 class Image {
 
     public: 
-        Image(int width, int height);
+        Image(unsigned int width, unsigned int height);
         ~Image(); 
 
     private:
         /** @brief Image width. */
-        int i_width; 
+        unsigned int i_width; 
         /** @brief Image height. */
-        int i_height;
-        /** @brief Memory buffer holding the actual image. */
-        unsigned char *i_buffer; 
+        unsigned int i_height;
+        /** @brief Number of bytes required to store a single pixel. */
+        
+        /** @brief 2D memory buffer holding the actual image. */
+        pixel_t **i_buffer; 
 };
 
 
--- src/image.cpp
+++ src/image.cpp
@@ -5,14 +5,23 @@
 
 #include "image.hpp"
 
-Image::Image(int width, int height) : i_width(width), i_height(height) {
-
-    
-
+Image::Image(unsigned int width, unsigned int height) : 
+            i_width(width), i_height(height) {
+ 
+    this->i_buffer = new pixel_t*[this->i_width];
+    for(unsigned int incr = 0; incr < this->i_width; incr++) {
+        
+        this->i_buffer[incr] = new pixel_t[this->i_height]; 
+    }
 }
 
 Image::~Image() {
 
+    for(unsigned int incr = 0; incr < this->i_width; incr++) {
+        
+        delete [] this->i_buffer[incr]; 
+    }
 
+    delete [] this->i_buffer; 
 }
 
