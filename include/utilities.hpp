a0c8eabe9410af6ee1a38250327d260b954066b2
Olivier Desenfans <desenfans.olivier@gmail.com>
2013-11-04 15:58:11 +0100

    Utilities: added the 'getFileExtension' function.
    This function returns the extension of a file as a std::string.

--- include/utilities.hpp
+++ include/utilities.hpp
@@ -11,7 +11,14 @@
 /** 
  * @brief Appends the given @p integer to the given @p string. 
  */
-void string_appendInt(std::string &str, int x); 
+void string_appendInt(std::string &str, int x);
+
+/**
+ * @brief Returns the extension of the given @p file. 
+ * @returns The file extension of the given @p file. If the file does not have an extension, 
+ *          the function returns an empty string. 
+ */
+std::string getFileExtension(std::string &filename); 
 
 #endif  /* DEF_UTILITIES_H */
 
