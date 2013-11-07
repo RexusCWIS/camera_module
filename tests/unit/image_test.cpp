62f3c2a7366478d132151add393551fd09d1e41e
Olivier Desenfans <desenfans.olivier@gmail.com>
2013-11-07 12:03:30 +0100

    Unit tests: started implementing a PNG comparison.
    NOT WORKING.

--- tests/unit/image_test.cpp
+++ tests/unit/image_test.cpp
@@ -13,7 +13,7 @@
 #define IMAGE_WIDTH     (800u)
 #define IMAGE_HEIGHT    (600u)
 
-bool isPNG(const char *pngFile); 
+bool isPNG(const char *pngFile);
 
 class ImageTest : public testing::Test {
     
@@ -31,6 +31,8 @@ class ImageTest : public testing::Test {
                 }
             }
         }
+
+        char* readPNG(const char *pngFile, unsigned int &width, unsigned int &height); 
 
         virtual void TearDown() {
             delete i_; 
@@ -70,5 +72,58 @@ bool isPNG(const char *pngFile) {
     fclose(fp);
     
     return(!png_sig_cmp(pngHeader, 0, 8)); 
+}
+
+
+char* ImageTest::readPNG(const char *pngFile, unsigned int &width, unsigned int &height) {
+
+    png_byte pngHeader[8]; 
+    png_structp png_ptr; 
+    png_infop info_ptr;
+
+    int bitDepth, colorType; 
+
+    char *image; 
+
+    FILE *fp = fopen(pngFile, "rb");
+
+    if(fp == NULL) {
+        return NULL; 
+    }
+    
+    fread(pngHeader, 1, sizeof(pngHeader), fp);
+    if(!png_check_sig(pngHeader, sizeof(pngHeader))) {
+
+        fclose(fp); 
+        return NULL; 
+    }
+
+    png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL); 
+    if(!png_ptr) {
+        return NULL; 
+    }
+
+    info_ptr = png_create_info_struct(png_ptr);
+    if(!info_ptr) {
+        png_destroy_read_struct(&png_ptr, NULL, NULL); 
+        return NULL;
+    }
+
+    png_init_io(png_ptr, fp); 
+
+    png_set_sig_bytes(png_ptr, sizeof(pngHeader)); 
+ 
+    bitDepth  = png_get_bit_depth(png_ptr, info_ptr); 
+    colorType = png_get_color_type(png_ptr, info_ptr); 
+
+    ASSERT_EQ(bitDepth, 8);
+
+    png_get_IHDR(png_ptr, info_ptr, (png_uint_32*) &width, (png_uint_32*) &height,
+                 NULL, NULL, NULL, NULL, NULL);
+
+    image = new char[width * height]; 
+    
+    png_destroy_read_struct(&png_ptr, NULL, NULL); 
+    fclose(fp); 
 }
 
