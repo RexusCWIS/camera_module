5952908bf7848da4f4cc9cb81851813659ecde0d
Olivier Desenfans <desenfans.olivier@gmail.com>
2013-11-13 01:28:29 +0100

    I2C: Created the 'I2CBus' class.
    This class manages the reception and handling of I2C messages.

---
+++ src/i2c/i2c_bus.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file i2c_bus.cpp
+ * @I2CBus class implementation.
+ */
+
+#include "i2c/i2c_bus.hpp"
+
+I2CBus::I2CBus(std::string tty) {
+
+    (void) tty;
+}
+
+I2CBus::~I2CBus() {
+    
+}
+
