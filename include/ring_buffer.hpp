4c9780debbcad12044975b37d5fb733923fa62cf
Olivier Desenfans <desenfans.olivier@gmail.com>
2013-11-18 11:26:45 +0100

    Ring Buffer: implemented the map between buffer addresses and Images.
    The 'getImageFromBuffer' method returns the address of an Image holding
    the given image buffer.

--- include/ring_buffer.hpp
+++ include/ring_buffer.hpp
@@ -11,18 +11,20 @@
 
 class RingBuffer {
     public: 
-        RingBuffer(size_t nbOfImages); 
+        RingBuffer(unsigned int width, unsigned int height, size_t nbOfImages); 
         ~RingBuffer();
 	    size_t getSize(void) const;
         Image * at(const size_t index) const;
-
+        Image * getImageFromBuffer(char * const buffer) const; 
+    
         Image*& operator[] (const size_t index); 
 
     private: 
         Image **m_imageArray;
-        size_t m_size; 
-        std::map<char *, Image *> m_bufferToImage; 
-
+        size_t m_size;
+        unsigned int m_width; 
+        unsigned int m_height; 
+        std::map<char * const, Image *> m_bufferToImage; 
 };
 
 #endif  /* DEF_RING_BUFFER_HPP */
--- src/ring_buffer.cpp
+++ src/ring_buffer.cpp
@@ -5,14 +5,25 @@
 
 #include "ring_buffer.hpp"
 
-RingBuffer::RingBuffer(size_t nbOfImages) :
-        m_size(nbOfImages) {
+RingBuffer::RingBuffer(unsigned int width, unsigned int height, size_t nbOfImages) :
+        m_size(nbOfImages), m_width(width), m_height(height) {
 
-    this->m_imageArray = new Image *[nbOfImages];  
+    this->m_imageArray = new Image *[nbOfImages]; 
+    
+    for(unsigned int incr = 0; incr < nbOfImages; incr++) {
+        this->m_imageArray[incr] = new Image(width, height); 
+        this->m_bufferToImage[this->m_imageArray[incr]->getImageBuffer()] = 
+                this->m_imageArray[incr];
+    }
 }
 
 RingBuffer::~RingBuffer() {
-    
+
+    for(unsigned int incr = 0; incr < this->m_size; incr++) {
+        delete m_imageArray[incr];
+        m_imageArray[incr] = NULL;
+    }
+
     delete [] this->m_imageArray; 
     m_imageArray = NULL; 
 }
@@ -25,6 +36,19 @@ Image * RingBuffer::at(const size_t index) const {
     
     if(index < this->m_size) {
         return m_imageArray[index];
+    }
+
+    return NULL; 
+}
+
+Image * RingBuffer::getImageFromBuffer(char * const buffer) const {
+
+    std::map<char * const, Image *>::const_iterator iter; 
+
+    iter = this->m_bufferToImage.find(buffer);
+    
+    if(iter != this->m_bufferToImage.end()) {
+        return iter->second; 
     }
 
     return NULL; 
