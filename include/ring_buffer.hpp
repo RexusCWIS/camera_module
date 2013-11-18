cad75068706908e7f0604aeecfb9749110962b32
Olivier Desenfans <desenfans.olivier@gmail.com>
2013-11-18 02:38:05 +0100

    Doxygen: documented the 'RingBuffer' class.

--- include/ring_buffer.hpp
+++ include/ring_buffer.hpp
@@ -9,18 +9,46 @@
 #include "image.hpp"
 #include <map>
 
+/**
+ * @brief Ring buffer for image acquisition.
+ * @details A ring buffer is a circular buffer holding image memory objects
+ *          for acquisition. Each frame acquired by the acquisition device
+ *          is stored sequentially in the buffer and overwrites images stored
+ *          during the previous ring buffer walk. 
+ */
 class RingBuffer {
-    public: 
-        RingBuffer(size_t nbOfImages); 
+    public:
+        /**
+         * @brief Builds a ring buffer of the given size. 
+         * @details Allocates memory for the given number of images and sets
+         *          the mapping between image buffers and the Image objects.
+         * @param[in]   nbOfImages  Number of images in the ring buffer. 
+         * @note Allocated memory is freed in @ref ~RingBuffer().
+         * @see ~RingBuffer()
+         */
+        RingBuffer(size_t nbOfImages);
+
+        /**
+         * @brief Tears down a ring buffer object.
+         * @details Frees the memory allocated for image objects.
+         */
         ~RingBuffer();
+
+        /** @brief Returns the number of images in the ring buffer. */
 	    size_t getSize(void) const;
-    
+   
+        /** @brief Returns a pointer to the image at the given @p index. */
         Image*& operator[] (const size_t index); 
         const Image*& operator[] (const size_t index) const; 
 
-    private: 
+    private:
+        /** @brief Image objects array. Holds the ring buffer data. */
         Image **m_imageArray;
+
+        /** @brief Size of the ring buffer, in images. */
         size_t m_ringBufferSize; 
+
+        /** @brief Maps an image buffer address to its corresponding Image object. */
         std::map<char *, Image *> m_bufferToImage; 
 
 };
