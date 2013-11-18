57fe02ec14001f23e24d2ec2387c3d8f72df0133
Olivier Desenfans <desenfans.olivier@gmail.com>
2013-11-18 12:09:55 +0100

    Unit tests: Added tests for the 'RingBuffer' class.

--- tests/unit/ring_buffer_test.cpp
+++ tests/unit/ring_buffer_test.cpp
@@ -10,18 +10,20 @@
 #include <stdlib.h>
 
 #define RING_BUFFER_SIZE    (27u)
+#define IMAGE_WIDTH         (800u)
+#define IMAGE_HEIGHT        (600u)
 
 class RingBufferTest : public testing::Test {
     
     protected: 
-        virtual void SetUp {
+        virtual void SetUp() {
 
-            rb_ = new RingBuffer(RING_BUFFER_SIZE); 
+            rb_ = new RingBuffer(IMAGE_WIDTH, IMAGE_HEIGHT, RING_BUFFER_SIZE); 
         }
 
-    virtual void TearDown() {
-        delete rb_; 
-    }
+        virtual void TearDown() {
+            delete rb_; 
+        }
 
     RingBuffer *rb_; 
 };
@@ -31,14 +33,23 @@ TEST_F(RingBufferTest, Constructors) {
     EXPECT_EQ(rb_->getSize(), RING_BUFFER_SIZE); 
 }
 
+TEST_F(RingBufferTest, Accessors) {
+
+    size_t rbSize = rb_->getSize(); 
+    ASSERT_EQ(rb_->at(rbSize + 1), (Image *) NULL); 
+
+    for(unsigned int incr = 0; incr < rbSize; incr++) {
+        ASSERT_EQ((*rb_)[incr], rb_->at(incr));  
+    }
+}
+
 TEST_F(RingBufferTest, MemoryBuffers) {
 
     Image *i;
 
     for(unsigned int incr = 0; incr < rb_->getSize(); incr++) {
-        
-        i = rb_[incr];
-        
+        i = (*rb_)[incr]; 
+        EXPECT_EQ(rb_->getImageFromBuffer(i->getImageBuffer()), i); 
     }
 }
 
