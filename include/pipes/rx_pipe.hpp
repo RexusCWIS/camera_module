227dbed63c4c5688aeba8aa23dc1d1a2994c9186
Olivier Desenfans <desenfans.olivier@gmail.com>
2013-11-21 21:20:08 +0100

    RXPipe: Implemented the skeleton of the threading mechanism.

--- include/pipes/rx_pipe.hpp
+++ include/pipes/rx_pipe.hpp
@@ -6,6 +6,7 @@
 #ifndef DEF_RX_PIPE_HPP
 #define DEF_RX_PIPE_HPP
 
+#include <pthread.h>
 #include <fstream>
 #include <string>
 
@@ -13,10 +14,19 @@ class RXPipe {
     public: 
         RXPipe(const std::string &pipefile, void (*rxCallback)(void));
         ~RXPipe(); 
+        
+        void start(void); 
+        void stop(void); 
 
     protected:
         std::filebuf *m_pipe;
         void (*m_callback)(void); 
+        bool m_running; 
+        bool m_stop;
+        pthread_t m_rxThread;
+        
+        static void * thread(void *arg);
+        void waitForThreadEnd(void); 
 };
 
 #endif  /* DEF_RX_PIPE_HPP */
--- src/pipes/rx_pipe.cpp
+++ src/pipes/rx_pipe.cpp
@@ -5,8 +5,12 @@
 
 #include "pipes/rx_pipe.hpp"
 
+/** @todo Implement error handling (open and close operations, ...) */
 RXPipe::RXPipe(const std::string &pipefile, void (*rxCallback)(void)) :
         m_callback(rxCallback) {
+
+    this->m_running = false; 
+    this->m_stop = false;
 
     this->m_pipe = new std::filebuf;
     this->m_pipe->open(pipefile.c_str(), std::ios_base::in); 
@@ -14,6 +18,36 @@ RXPipe::RXPipe(const std::string &pipefile, void (*rxCallback)(void)) :
 
 RXPipe::~RXPipe() {
 
+    this->m_pipe->close(); 
     delete this->m_pipe; 
+}
+
+void RXPipe::start(void) {
+
+    this->m_running = true;
+    pthread_create(&(this->m_rxThread), NULL, &thread, this); 
+    
+}
+
+void RXPipe::stop(void) {
+
+    this->m_stop = true; 
+    this->waitForThreadEnd(); 
+}
+
+void * RXPipe::thread(void *arg) {
+
+    RXPipe *rxPipe = reinterpret_cast<RXPipe *>(arg);
+
+    while(!rxPipe->m_stop) {
+
+    }
+
+    return NULL;
+}
+
+void RXPipe::waitForThreadEnd(void) {
+
+    pthread_join(this->m_rxThread, NULL); 
 }
 
