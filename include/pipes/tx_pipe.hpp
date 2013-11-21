4dbef8f3e7e15e1f724644a140748dc45034e389
Olivier Desenfans <desenfans.olivier@gmail.com>
2013-11-21 23:26:24 +0100

    Implemented the new 'TXPipe' class.
    This class is a wrapper around the std::filebuf class to write data to
    a pipe.

---
+++ include/pipes/tx_pipe.hpp
@@ -0,0 +1,23 @@
+/**
+ * @file tx_pipe.hpp 
+ * @brief Transmitter pipe class definition.
+ */
+
+#ifndef DEF_TX_PIPE_HPP
+#define DEF_TX_PIPE_HPP
+
+#include <fstream>
+#include <string>
+
+class TXPipe {
+    public: 
+        TXPipe(const std::string &pipefile); 
+        ~TXPipe();
+
+        void send(char *data, int dataSize);
+    private:
+        std::filebuf *m_pipe; 
+};
+
+#endif  /* DEF_TX_PIPE_HPP */
+
