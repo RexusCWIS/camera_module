75dac365b51c7de955557efff8169fcc91116841
Olivier Desenfans <desenfans.olivier@gmail.com>
2013-11-22 17:22:53 +0100

    Serial bus classes: fixed include directives.
    Some include directives were causing compilation errors after changing
    the directory name.

--- src/serial/rx_thread.cpp
+++ src/serial/rx_thread.cpp
@@ -3,7 +3,7 @@
  * @brief I2C message reception handler thread implementation.
  */
 
-#include "i2c/rx_thread.hpp"
+#include "serial/rx_thread.hpp"
 
 RXThread::RXThread(I2CBus bus) : m_bus(bus) {
 
