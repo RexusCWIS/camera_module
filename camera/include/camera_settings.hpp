abdf6df4214bf7eb36493f546af974ff7e847da8
Olivier Desenfans <desenfans.olivier@gmail.com>
2013-12-01 22:12:11 +0100

    Experiment: added camera configuration parameters.

---
+++ camera/include/camera_settings.hpp
@@ -0,0 +1,15 @@
+/**
+ * @file camera_settings.hpp
+ * @brief Camera settings. 
+ */
+
+#ifndef DEF_CAMERA_SETTINGS_HPP
+#define DEF_CAMERA_SETTINGS_HPP
+
+#define AOI_WIDTH   3400u
+#define AOI_HEIGHT  1300u
+#define AOI_H_OFFSET    190u
+#define AOI_V_OFFSET    290u
+
+#endif  /* DEF_CAMERA_SETTINGS_HPP */
+
