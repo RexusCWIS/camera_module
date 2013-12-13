925bb762476cef1998927575e43d9b2f2cb48183
Olivier Desenfans <desenfans.olivier@gmail.com>
2013-12-13 03:40:06 +0100

    I2C-com: Modiified the application to process and transfer camera orders.

--- i2c_com/main.cpp
+++ i2c_com/main.cpp
@@ -11,36 +11,41 @@
 #include <termios.h>
 #include <unistd.h>
 
+#include "pipes/tx_pipe.hpp"
+
 #include "usbiss.h"
 
 static void display_version(void); 
 
-static unsigned char tx_buf[32] = "I love those incredible buffers";
 static unsigned char rx_buf[32]; 
 
 int main(int argc, char *argv[]) {
+
+    TXPipe camera_tx("/tmp/camera_pipe.p"); 
+    unsigned char current_order = 'S'; 
 
 	iss_init("/dev/ttyACM0"); 
 
 	display_version(); 	
 
 	iss_set_mode(I2C_H_400KHZ, IO_MODE_DIGITAL_INPUT);
+
+    while(true) {
+
+        /* Read the camera order */
+	    iss_i2c_read(rx_buf, 0x1u, 0x23u, 0x0u); 
+        if(rx_buf[0] != current_order) {
+            camera_tx.send((const char *) rx_buf, 1); 
+        }
+
+	    iss_i2c_read(rx_buf, 0xCu, 0x23u, 0x1u); 
 	
-//	iss_i2c_write(tx_buf, 32u, 0x22u, 0x0u);
+	    unsigned int time = (((unsigned int) rx_buf[1]) << 8) + rx_buf[0]; 
+	    std::cout << "Current experiment time: " << time << std::endl; 
 
-//	sleep(1); 
-
-	iss_i2c_read(rx_buf, 0x1u, 0x23u, 0x0u); 
-
-	std::cout << "Camera order: " << rx_buf[0] << std::endl; 
-
-	iss_i2c_read(rx_buf, 0xCu, 0x23u, 0x1u); 
-	
-	unsigned int time = (((unsigned int) rx_buf[1]) << 8) + rx_buf[0]; 
-	std::cout << "Current experiment time: " << time << std::endl; 
+    }
 
     iss_close(); 
-
 	exit(EXIT_SUCCESS); 
 }
 
