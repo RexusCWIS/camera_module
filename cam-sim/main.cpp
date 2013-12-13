23382980e722ae33688e9e854b4f724e5cd59765
Olivier Desenfans <desenfans.olivier@gmail.com>
2013-12-13 03:47:49 +0100

    Added a camera process simulator.

---
+++ cam-sim/main.cpp
@@ -0,0 +1,37 @@
+/**
+ * @file main.cpp
+ * @brief I2C communication process.
+ */
+
+#include <iostream>
+
+#include "pipes/rx_pipe.hpp"
+
+static void order_processing(char orders[], int size); 
+
+int main(int argc, char *argv[]) {
+
+    RXPipe camera_rx("/tmp/camera_pipe.p", &order_processing); 
+    camera_rx.start(); 
+
+    while(true) {
+
+    }
+	exit(EXIT_SUCCESS); 
+}
+
+static void order_processing(char orders[], int size) {
+
+    switch(orders[0]) {
+        
+        case 'G': 
+            std::cout << "The experiment has STARTED." << std::endl; 
+            break; 
+        case 'S':
+            std::cout << "The experiment is OVER." << std::endl; 
+            break; 
+        default: 
+            std::cout << "Unknown order." << std::endl; 
+    }
+}
+
